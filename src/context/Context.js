04c41a731543f4dfae3e436d391c1733b8e69e2b
InderParmar <iparmar1@myseneca.ca>
2024-03-15 19:26:15 -0400

    Merge remote changes

---
+++ src/context/Context.js
@@ -0,0 +1,44 @@
+import faker from "faker";
+import { createContext, useContext, useReducer } from "react";
+import { cartReducer, productReducer } from "./Reducers";
+
+const Cart = createContext();
+
+const Context = ({ children }) => {
+  faker.seed(99)
+
+  const products = [...Array(20)].map(() =>({
+    id:faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.image(),
+    inStock: faker.random.arrayElement([0,3,5,6,7]),
+    fastDelivery: faker.datatype.boolean(),
+    ratings: faker.random.arrayElement([1,2,3,4,5])
+  }))
+
+const [state, dispatch] = useReducer(cartReducer, {
+  products: products,
+  cart: []
+})
+
+const [productState, productDispatch] = useReducer(productReducer, {
+  byStock: false,
+  byFastDelivery: false,
+  byRating: 0,
+  searchQuery:''
+})
+
+  return (
+    <Cart.Provider value={{state, dispatch, productState, productDispatch}}>
+      { children }
+    </Cart.Provider >
+  )
+}
+
+export default Context
+
+
+export const CartState = () => {
+  return useContext(Cart)
+}
